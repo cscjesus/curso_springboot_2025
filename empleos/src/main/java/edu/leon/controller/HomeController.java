a95acfc8b4e82e8fe663c19349a8039e5db7112d
Jesus hdez <csc.jesus@gmail.com>
2025-07-07 10:08:42 -0600

    Agregando tipos de datos simples al modelo

--- empleos/src/main/java/edu/leon/controller/HomeController.java
+++ empleos/src/main/java/edu/leon/controller/HomeController.java
@@ -4,14 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String mostrartHome(Model model){
-        model.addAttribute("mensaje","Bienvenidos a Empleos App");
-        model.addAttribute("fecha", LocalDateTime.now());
+//        model.addAttribute("mensaje","Bienvenidos a Empleos App");
+//        model.addAttribute("fecha", LocalDateTime.now());
+        String nombre = "Auxiliar de Contabilidad";
+        LocalDate fechaPub = LocalDate.now();
+        double salario = 9000;
+        boolean vigente = true;
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("fecha", fechaPub);
+        model.addAttribute("salario", salario);
+        model.addAttribute("vigente", vigente);
         return "home";
+        //seccion 2 video 8
     }
 }

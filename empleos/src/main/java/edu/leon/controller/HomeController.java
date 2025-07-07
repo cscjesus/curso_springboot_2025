438bb3b1ad016b73b887e513f5afd37a63e453f6
Jesus hdez <csc.jesus@gmail.com>
2025-07-07 09:32:03 -0600

    thymeleaf agregado y mostrando en html

--- empleos/src/main/java/edu/leon/controller/HomeController.java
+++ empleos/src/main/java/edu/leon/controller/HomeController.java
@@ -1,12 +1,17 @@
 package edu.leon.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String mostrartHome(){
+    public String mostrartHome(Model model){
+        model.addAttribute("mensaje","Bienvenidos a Empleos App");
+        model.addAttribute("fecha", LocalDateTime.now());
         return "home";
     }
 }

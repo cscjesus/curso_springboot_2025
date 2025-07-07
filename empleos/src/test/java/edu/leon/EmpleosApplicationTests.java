ab546c367be9140382fa7e0db185524d8c4297c6
Jesus hdez <csc.jesus@gmail.com>
2025-07-07 08:27:34 -0600

    Inicio proyecto empleos

---
+++ empleos/src/test/java/edu/leon/EmpleosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.leon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

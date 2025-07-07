2497fd935e92e588e632db173bb05e43b7f79c12
Jesus hdez <csc.jesus@gmail.com>
2025-07-07 10:53:45 -0600

    Mostrando listas con thymeleaf

--- empleos/src/main/java/edu/leon/controller/HomeController.java
+++ empleos/src/main/java/edu/leon/controller/HomeController.java
@@ -6,9 +6,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    @GetMapping("/listado")
+    public String mostrarListado(Model model){
+        List<String> lista = new LinkedList<>();
+        lista.add("Ingeniero de Sistemas");
+        lista.add("Auxiliar de Contabilidad");
+        lista.add("Vendedor");
+        lista.add("Arquitecto");
+
+        model.addAttribute("empleos",lista);
+
+        return "listado";
+    }
     @GetMapping("/")
     public String mostrartHome(Model model){
 //        model.addAttribute("mensaje","Bienvenidos a Empleos App");
@@ -22,6 +36,6 @@ public class HomeController {
         model.addAttribute("salario", salario);
         model.addAttribute("vigente", vigente);
         return "home";
-        //seccion 2 video 8
+        //seccion 2 video 9
     }
 }

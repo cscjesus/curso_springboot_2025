9212c040bca3f33d52e09689001bb4ef2d96f84e
Jesus hdez <csc.jesus@gmail.com>
2025-07-07 08:13:46 -0600

    Inicio del curso

---
+++ holaMundo/src/main/java/edu/leon/holaMundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package edu.leon.holaMundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaMundoApplication {
+//secccion 01 video 07
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}

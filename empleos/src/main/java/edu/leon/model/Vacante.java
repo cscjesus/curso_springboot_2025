8a60845c5e1a5cc85d27f1fa2007ed4b024a181f
Jesus hdez <csc.jesus@gmail.com>
2025-07-09 10:11:38 -0600

    Condicionales en Thymelef

--- empleos/src/main/java/edu/leon/model/Vacante.java
+++ empleos/src/main/java/edu/leon/model/Vacante.java
@@ -2,8 +2,8 @@ package edu.leon.model;
 
 import java.time.LocalDate;
 
-public record Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario) {
+public record Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado) {
    public Vacante(){
-       this(null,null,null,null,null);
+       this(null,null,null,null,null,null);
    }
 }

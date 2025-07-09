ea4fee1aee35cb644a58af0c631c0a9b5d2d52bb
Jesus hdez <csc.jesus@gmail.com>
2025-07-09 11:10:23 -0600

    Agregar imagenes a la lista de Vacantes

--- empleos/src/main/java/edu/leon/controller/HomeController.java
+++ empleos/src/main/java/edu/leon/controller/HomeController.java
@@ -22,7 +22,7 @@ public class HomeController {
 
     @GetMapping("/detalle")
     public String mostrarDetalle(Model model) {
-        Vacante vacante = new Vacante(0, "Ingeniero de Comunicaciones", "Se solicita Ing. para dar soporte a intranet", LocalDate.now(), 9700.0,0);
+        Vacante vacante = new Vacante(0, "Ingeniero de Comunicaciones", "Se solicita Ing. para dar soporte a intranet", LocalDate.now(), 9700.0,0,"no-omage.png");
         model.addAttribute("vacante", vacante);
         return "detalle";
     }
@@ -63,16 +63,17 @@ public class HomeController {
         List<Vacante> vacantes = new LinkedList<>();
         DateTimeFormatter df = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         var vacante1 = new Vacante(1,"Ingeniero Civil","Se solicita Ing. Civil para " +
-                "disenar puente peatonal",LocalDate.parse("08-02-2019",df),8500.0,1);
+                "disenar puente peatonal",LocalDate.parse("08-02-2019",df),8500.0,1,"empresa1.png");
         vacantes.add(vacante1);
 
-        vacante1 = new Vacante(2,"Contador Publico","Empresa importante solicita contandor con 5 anios de experiencia titulado",LocalDate.parse("09-02-2019",df),12000.0,0);
+        vacante1 = new Vacante(2,"Contador Publico","Empresa importante solicita contandor con 5 anios de experiencia titulado",LocalDate.parse("09-02-2019",df),12000.0,0,"empresa2.png");
         vacantes.add(vacante1);
 
         vacante1 = new Vacante(3,"Ingeniero Electrico","Empresa internacional solicita Ingeniero Electrico para mantenimiento de instalacion electrica",LocalDate.parse("10-02-2019",df),10500.0,0);
         vacantes.add(vacante1);
 
-        vacante1 = new Vacante(4,"Disenador Grafico","Solicitamos Disenador Grafico titulado para disenar publicidad de la empresa",LocalDate.parse("01-02-2019",df),7500.0,1);
+        vacante1 = new Vacante(4,"Disenador Grafico","Solicitamos Disenador Grafico titulado para disenar publicidad de la empresa",LocalDate.parse("01-02-2019",df),7500.0,1,"empresa3.png");
+
         vacantes.add(vacante1);
 
         return vacantes;
--- empleos/src/main/java/edu/leon/model/Vacante.java
+++ empleos/src/main/java/edu/leon/model/Vacante.java
@@ -2,8 +2,11 @@ package edu.leon.model;
 
 import java.time.LocalDate;
 
-public record Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado) {
+public record Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado,String imagen) {
    public Vacante(){
-       this(null,null,null,null,null,null);
+       this(null,null,null,null,null,null,"no-image.png");
+   }
+   public Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado){
+       this(id,nombre,descripcion,fecha,salario,destacado,"no-image.png");
    }
 }

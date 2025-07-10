61b0835b40a893f3b795198759ebf86208682497
Jesus hdez <csc.jesus@gmail.com>
2025-07-10 09:03:23 -0600

    Utilizar RequestMapping a nivel de clase

--- empleos/src/main/java/edu/leon/EmpleosApplication.java
+++ empleos/src/main/java/edu/leon/EmpleosApplication.java
@@ -9,6 +9,6 @@ public class EmpleosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
-	//seccion 2 video 18
+	//seccion 3 video 3
 
 }
---
+++ empleos/src/main/java/edu/leon/controller/CategoriasController.java
@@ -0,0 +1,24 @@
+package edu.leon.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/categorias")
+public class CategoriasController {
+    @GetMapping("/index")
+    public String mostrarIndex(Model model) {
+        return "categorias/listCategorias";
+    }
+    @GetMapping("/create")
+    public String crear(){
+        return  "categorias/formCategoria";
+    }
+    @PostMapping("/save")
+    public String guardar(){
+        return "categorias/listCategoria";
+    }
+}

cc320f1d413759ad7c446b77af7e8aecc622ba4e
Jesus hdez <csc.jesus@gmail.com>
2025-07-14 10:51:43 -0600

    Ejemplo peticion post

--- empleos/src/main/java/edu/leon/EmpleosApplication.java
+++ empleos/src/main/java/edu/leon/EmpleosApplication.java
@@ -9,6 +9,6 @@ public class EmpleosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
-	//seccion 3 video 8
+	//seccion 4 video 0
 
 }
--- empleos/src/main/java/edu/leon/controller/CategoriasController.java
+++ empleos/src/main/java/edu/leon/controller/CategoriasController.java
@@ -2,10 +2,7 @@ package edu.leon.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/categorias")
@@ -18,10 +15,15 @@ public class CategoriasController {
     public String crear(){
         return  "categorias/formCategoria";
     }
+
+
     @PostMapping("/save")
-    public String guardar(){
-        return "categorias/listCategoria";
+    public String guardar(@RequestParam("nombre") String nombre,@RequestParam("descripcion") String descripcion){
+        System.out.println("Categoria:"+nombre);
+        System.out.println("Descripcion:"+descripcion);
+        return "categorias/listCategorias";
     }
+
     @GetMapping("/detalle/{id}")
     public String mostrarDetalle(@PathVariable("id") int idVacante){
         System.out.println("PathVariable: "+idVacante);

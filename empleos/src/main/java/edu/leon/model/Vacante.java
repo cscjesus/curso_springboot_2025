a656d6b93d15c4e98b8b762526447d0604f39ba9
Jesus hdez <csc.jesus@gmail.com>
2025-07-15 13:02:55 -0600

    Agregar ver detalle.html con bootstrap

--- empleos/src/main/java/edu/leon/model/Vacante.java
+++ empleos/src/main/java/edu/leon/model/Vacante.java
@@ -1,6 +1,7 @@
 package edu.leon.model;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public record Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado,String imagen) {
    public Vacante(){
@@ -8,5 +9,8 @@ public record Vacante(Integer id, String nombre, String descripcion, LocalDate f
    }
    public Vacante(Integer id, String nombre, String descripcion, LocalDate fecha,Double salario,Integer destacado){
        this(id,nombre,descripcion,fecha,salario,destacado,"no-image.png");
+   }
+   public String fechaFormat(){
+       return fecha.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")).toString();
    }
 }

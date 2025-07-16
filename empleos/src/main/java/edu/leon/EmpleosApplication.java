97c3e10ef7ae06340aaf9095814c20168ae5f286
Jesus hdez <csc.jesus@gmail.com>
2025-07-16 09:21:43 -0600

    Agregar formCategoria.html

--- empleos/src/main/java/edu/leon/EmpleosApplication.java
+++ empleos/src/main/java/edu/leon/EmpleosApplication.java
@@ -9,6 +9,6 @@ public class EmpleosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmpleosApplication.class, args);
 	}
-	//seccion 5 video 3
+	//seccion 5 video 5
 
 }

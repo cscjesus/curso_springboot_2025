a7013c3a612cd823b3ae597905ad6b4624846989
Jesus hdez <csc.jesus@gmail.com>
2025-07-19 11:51:14 -0600

    Inyectando CategoriasRepository

--- jpa-demo/src/main/java/edu/leon/JpaDemoApplication.java
+++ jpa-demo/src/main/java/edu/leon/JpaDemoApplication.java
@@ -1,24 +1,29 @@
 package edu.leon;
 
+import edu.leon.respositories.CategoriasRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JpaDemoApplication implements CommandLineRunner {
-
+    @Autowired
+    private CategoriasRepository categoriasRepo;
     public static void main(String[] args) {
         SpringApplication.run(JpaDemoApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-//		System.out.println("Ejemplos de Spring Data JPA");
-        guardar();
+        System.out.println(categoriasRepo);
     }
 
     private void guardar() {
         System.out.println("Insertando ...");
     }
-//seccion 8 video 9
+    private void eliminar() {
+        System.out.println("Elimando ...");
+    }
+//seccion 8 video 12
 }
---
+++ jpa-demo/src/main/java/edu/leon/respositories/CategoriasRepository.java
@@ -0,0 +1,7 @@
+package edu.leon.respositories;
+
+import edu.leon.model.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriasRepository extends CrudRepository<Categoria,Integer> {
+}

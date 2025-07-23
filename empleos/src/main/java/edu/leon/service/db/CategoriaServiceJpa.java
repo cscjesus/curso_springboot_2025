a9501e73f787333d872252643af1121869a82c1b
Jesus hdez <csc.jesus@gmail.com>
2025-07-23 07:53:09 -0600

    Crear nueva implementacion de ICategoriaService para trabajar con BD

---
+++ empleos/src/main/java/edu/leon/service/db/CategoriaServiceJpa.java
@@ -0,0 +1,32 @@
+package edu.leon.service.db;
+
+import edu.leon.model.Categoria;
+import edu.leon.repositories.CategoriasRepository;
+import edu.leon.service.ICategoriasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoriaServiceJpa implements ICategoriasService {
+
+    @Autowired
+    private CategoriasRepository categoriasRepository;
+
+    @Override
+    public void guardar(Categoria categoria) {
+    categoriasRepository.save(categoria);
+    }
+
+    @Override
+    public List<Categoria> buscarTodas() {
+        return categoriasRepository.findAll();
+    }
+
+    @Override
+    public Categoria buscarPorId(Integer idCategoria) {
+        return categoriasRepository.findById(idCategoria)
+                .orElse(null);
+    }
+}

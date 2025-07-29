5a8e7873af2d1a919d2a120e54028454e2e894dd
Jesus hdez <csc.jesus@gmail.com>
2025-07-29 09:23:14 -0600

    Esquemas de usuarios y roles persozanilzados para Sring Security. Junto con  DatabaseWebSecurity.java

--- empleos/src/main/java/edu/leon/security/DatabaseWebSecurity.java
+++ empleos/src/main/java/edu/leon/security/DatabaseWebSecurity.java
@@ -12,9 +12,22 @@ import javax.sql.DataSource;
 @EnableWebSecurity
 public class DatabaseWebSecurity {
 
-    @Bean
+    //tablas por defecto
+    /*@Bean
     UserDetailsManager users(DataSource dataSource){//se inyecta  desde application.properties
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+
+        return users;
+    }*/
+    //tablas personalizadas
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.setUsersByUsernameQuery("select username, password, estatus from Usuarios where username=?");
+        users.setAuthoritiesByUsernameQuery("select u.username, p.perfil from UsuarioPerfil up " +
+                "inner join Usuarios u on   u.id = up.idUsuario " +
+                "inner join Perfiles p on   p.id = up.idPerfil " +
+                "where u.username=?");
         return users;
     }
 }

753dd17eba8848d844918f685d7bee5aeed32e64
Jesus hdez <csc.jesus@gmail.com>
2025-08-01 11:54:56 -0600

    Guardar archivo CV en el disco duro

--- empleos/src/main/java/edu/leon/controller/SolicitudesController.java
+++ empleos/src/main/java/edu/leon/controller/SolicitudesController.java
@@ -4,14 +4,14 @@ import edu.leon.model.Solicitud;
 import edu.leon.model.Vacante;
 import edu.leon.services.IVacanteService;
 import edu.leon.services.db.VacantesServiceJpa;
+import edu.leon.util.Utileria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/solicitudes")
@@ -24,7 +24,7 @@ public class SolicitudesController {
     IVacanteService vacantesService;
 
     @GetMapping("/create/{idVacante}")
-    public String crear(Solicitud solicitud, @PathVariable Integer idVacante, Model model){
+    public String crear(Solicitud solicitud, @PathVariable Integer idVacante, Model model) {
         Vacante vacante = vacantesService.buscarPorId(idVacante);
         model.addAttribute("vacante", vacante);
 //        solicitud.setVacante(vacante);
@@ -33,7 +33,20 @@ public class SolicitudesController {
     }
 
     @PostMapping("/save")
-    public String guardar(Solicitud solicitud){
+    public String guardar(Solicitud solicitud, BindingResult bindingResult, @RequestParam("archivoCV") MultipartFile multipart) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("Existen errores en el formulario");
+            return "solicitudes/formSolicitud";
+        }
+        if (!multipart.isEmpty()) {
+            String nombreArchivo = Utileria.guardarArchivo(multipart, rutaCv);
+
+            if (nombreArchivo != null) {
+                solicitud.setArchivo(nombreArchivo);
+            } else {
+                System.out.println("Error al guardar el archivo");
+            }
+        }
         System.out.println("Solicitud: " + solicitud);
         return "redirect:/";
     }

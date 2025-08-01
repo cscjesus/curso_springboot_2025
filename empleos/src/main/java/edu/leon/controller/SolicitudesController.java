900008327c169676320dfd3c20586095ab0bc7d7
Jesus hdez <csc.jesus@gmail.com>
2025-08-01 08:13:05 -0600

    Configurar formSolicitud.html

--- empleos/src/main/java/edu/leon/controller/SolicitudesController.java
+++ empleos/src/main/java/edu/leon/controller/SolicitudesController.java
@@ -1,5 +1,9 @@
 package edu.leon.controller;
 
+import edu.leon.model.Vacante;
+import edu.leon.services.IVacanteService;
+import edu.leon.services.db.VacantesServiceJpa;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/solicitudes")
 public class SolicitudesController {
+    @Autowired
+    IVacanteService vacantesService;
+
     @GetMapping("/create/{idVacante}")
     public String crear(@PathVariable Integer idVacante, Model model){
+        Vacante vacante = vacantesService.buscarPorId(idVacante);
+        model.addAttribute("vacante", vacante);
+
         System.out.println(idVacante);
         return "solicitudes/formSolicitud";
     }

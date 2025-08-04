dbf3184f167d899c17984ff1ef11b3c408612550
Jesus hdez <csc.jesus@gmail.com>
2025-08-04 13:09:48 -0600

    Crear el rest controller AlbumsController

---
+++ music-api/src/main/java/edu/leon/api/controller/AlbumsController.java
@@ -0,0 +1,23 @@
+package edu.leon.api.controller;
+
+
+import edu.leon.api.entity.Album;
+import edu.leon.api.service.IAlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api")
+public class AlbumsController {
+    @Autowired
+    private IAlbumService albumService;
+
+    @GetMapping("/albums")
+    public List<Album> buscarTodos() {
+        return albumService.buscarTodos();
+    }
+}

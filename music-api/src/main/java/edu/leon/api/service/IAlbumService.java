6fd41af465a16275b6dab76049e7671dae3d042a
Jesus hdez <csc.jesus@gmail.com>
2025-08-04 12:54:18 -0600

    Crear IAlbumService y AlbumsService

---
+++ music-api/src/main/java/edu/leon/api/service/IAlbumService.java
@@ -0,0 +1,9 @@
+package edu.leon.api.service;
+
+import edu.leon.api.entity.Album;
+
+import java.util.List;
+
+public interface IAlbumService {
+    List<Album> buscarTodos();
+}
---
+++ music-api/src/main/java/edu/leon/api/service/jpa/AlbumsService.java
@@ -0,0 +1,20 @@
+package edu.leon.api.service.jpa;
+
+import edu.leon.api.entity.Album;
+import edu.leon.api.repository.AlbumsRepository;
+import edu.leon.api.service.IAlbumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumsService implements IAlbumService {
+    @Autowired
+    private AlbumsRepository albumsRepository;
+
+    @Override
+    public List<Album> buscarTodos() {
+        return albumsRepository.findAll();
+    }
+}
